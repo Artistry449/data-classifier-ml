56ac38aab55acfd1931f690c06779e770c804218
Tuguldur <tt2703633@gmail.com>
2024-11-26 16:50:27 +0800

    word char count

--- app/page.tsx
+++ app/page.tsx
@@ -37,20 +37,28 @@ const HomePage = () => {
     setText("");
   };
 
+  const handleCheck = () => {
+
+  }
+
+  const wordCount = text.trim().split(/\s+/).filter((word) => word).length;
+  const charCount = text.replace(/\s/g, "").length;
+
   return (
     <div className="bg-gray-50 flex items-center justify-center h-screen">
       <div className="flex flex-col md:flex-row w-full h-full max-w-6xl max-h-96">
         <div className="w-full flex h-full">
           <div className="flex flex-col space-y-2 mr-5">
-            <button
+          <button
               onClick={handlePaste}
-              className={`bg-gray-100 p-2 rounded hover:bg-gray-200 ${
+              className={`bg-gray-100 p-1 rounded hover:bg-gray-200 ${
                 !isClipboardAvailable ? "cursor-not-allowed opacity-50" : ""
               }`}
               disabled={!isClipboardAvailable}
             >
-              <img src="paste-regular.svg" />
-            </button>
+              <img src="paste-regular.svg" alt="Paste" className="w-10 h-10" />
+          </button>
+
             <button
               onClick={handleCopy}
               className={`bg-gray-100 p-2 rounded hover:bg-gray-200 ${
@@ -58,7 +66,7 @@ const HomePage = () => {
               }`}
               disabled={!isClipboardAvailable}
             >
-              <img src="copy-regular.svg" />
+              <img src="copy-regular.svg" alt="Copy" className="w-10 h-10" />
             </button>
             <button
               onClick={handleClear}
@@ -66,15 +74,42 @@ const HomePage = () => {
             >
               Clear
             </button>
+
+            <button
+              onClick={handleCheck}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded "
+            >
+              Алдааг шалгах
+            </button>
           </div>
           <div className="flex flex-col w-full border border-gray-300 rounded resize-none bg-white rounded-lg shadow p-6 h-full">
-            <textarea
+            {/* <textarea
               placeholder="Type your text here..."
               value={text}
               onChange={(e) => setText(e.target.value)}
+              maxLength={10}
               className="w-full h-full outline-none"
-            />
+            /> */}
+            <textarea
+  placeholder="Type your text here..."
+  value={text}
+  onChange={(e) => {
+    const input = e.target.value;
+    setText(input.slice(0, 1200)); 
+  }}
+  maxLength={1200} 
+  className="w-full h-full outline-none"
+/>
+
+        <div className="flex justify-between mt-4 text-sm text-gray-600">
+            <div>Үгийн тоо: {wordCount}</div>
+            <div>
+              Тэмдэгтийн тоо: {charCount}/1200
+            </div>
           </div>
+          </div>
+          
+
         </div>
 
         <div className="w-full md:w-1/4 ml-5 border-gray-200 p-4 bg-white rounded-lg shadow p-6">
