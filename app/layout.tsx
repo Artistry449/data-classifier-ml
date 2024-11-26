be5e35a79897669fe53fda9d82d4c7b04baa9c80
boroonoo_xvn <esonkhastdorjpurev@gmail.com>
2024-11-26 17:41:18 +0800

    fixed ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { GiMongolia } from "react-icons/gi";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,9 +27,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
+        <header className="flex justify-between max-w-4xl m-auto h-14 items-center p-8 " >
+          <div>
+            <GiMongolia size={60}/>
+            {/* <Image src={"https://www.vectorkhazana.com/assets/images/products/Super-Mario-Strikers.jpg"} alt="it's me mario" height={30} width={30}/> */}
+            {/* <h1>Algo biy daalt 1</h1> */}
+          </div>
+          <div className=" font-bold">
+            Хамтын хүч оломгүй далай
+          </div>
+        </header>
         {children}
+        <footer className="flex justify-between max-w-4xl m-auto p-8 h-14">
+          <div>
+            <p>
+              Powered by Students
+            </p>
+            <p className=" text-sm h-14">
+              © 2024 - ШУТИС МХТС оюутнууд
+            </p>
+          </div>
+          <div>
+
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { FaRegPaste, FaRegCopy } from "react-icons/fa6";
+import { MdOutlineDeleteForever } from "react-icons/md";
+
 
 const HomePage = () => {
   const [text, setText] = useState("");
   const [isClipboardAvailable, setClipboardAvailable] = useState(false);
 
   useEffect(() => {
-    if (typeof window !== "undefined" && navigator.clipboard) {
-      setClipboardAvailable(true);
-    }
+    setClipboardAvailable(typeof navigator !== "undefined" && !!navigator.clipboard);
   }, []);
 
   const handlePaste = async () => {
@@ -45,18 +46,18 @@ const HomePage = () => {
   const charCount = text.replace(/\s/g, "").length;
 
   return (
-    <div className="bg-gray-50 flex items-center justify-center h-screen">
+    <div className="bg-yellow-400 flex items-center h-[500px] justify-center">
       <div className="flex flex-col md:flex-row w-full h-full max-w-6xl max-h-96">
         <div className="w-full flex h-full">
-          <div className="flex flex-col space-y-2 mr-5">
+          <div className="flex flex-col space-y-2 mr-3">
           <button
               onClick={handlePaste}
-              className={`bg-gray-100 p-1 rounded hover:bg-gray-200 ${
+              className={`bg-gray-100 p-2 rounded hover:bg-gray-200 ${
                 !isClipboardAvailable ? "cursor-not-allowed opacity-50" : ""
               }`}
               disabled={!isClipboardAvailable}
             >
-              <img src="paste-regular.svg" alt="Paste" className="w-10 h-10" />
+              <FaRegPaste size={20}/>
           </button>
 
             <button
@@ -66,32 +67,18 @@ const HomePage = () => {
               }`}
               disabled={!isClipboardAvailable}
             >
-              <img src="copy-regular.svg" alt="Copy" className="w-10 h-10" />
+              <FaRegCopy size={20}/>
             </button>
             <button
               onClick={handleClear}
               className="bg-gray-100 p-2 rounded hover:bg-gray-200"
             >
-              Clear
-            </button>
-
-            <button
-              onClick={handleCheck}
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded "
-            >
-              Алдааг шалгах
+              <MdOutlineDeleteForever size={20}/>
             </button>
           </div>
-          <div className="flex flex-col w-full border border-gray-300 rounded resize-none bg-white rounded-lg shadow p-6 h-full">
-            {/* <textarea
-              placeholder="Type your text here..."
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              maxLength={10}
-              className="w-full h-full outline-none"
-            /> */}
+          <div className="flex flex-col w-full border border-gray-300 resize-none bg-white rounded-lg shadow p-6 h-full">
             <textarea
-                placeholder="Type your text here..."
+                placeholder="Текстээ оруулна уу..."
                 value={text}
                 onChange={(e) => {
                   const input = e.target.value;
@@ -101,19 +88,26 @@ const HomePage = () => {
                 className="w-full h-full outline-none"
               />
 
-        <div className="flex justify-between mt-4 text-sm text-gray-600">
+        <div className="flex justify-between mt-4 text-sm text-gray-600 items-center">
             <div>Үгийн тоо: {wordCount}</div>
-            <div>
+            <div >
               Тэмдэгтийн тоо: {charCount}/1200
+            <button
+              onClick={handleCheck}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border-green-700 rounded-xl ml-4"
+            >
+              Алдааг шалгах
+            </button>
             </div>
+            
           </div>
           </div>
           
 
         </div>
 
-        <div className="w-full md:w-1/4 ml-5 border-gray-200 p-4 bg-white rounded-lg shadow p-6">
-          <h2 className="text-lg font-semibold">Алдаатай үгсийн жагсаалт</h2>
+        <div className="w-full md:w-1/4 ml-5 border-gray-200 bg-white rounded-lg shadow p-6">
+          <h2 className="text-sm font-semibold">Алдаатай үгсийн жагсаалт</h2>
         </div>
       </div>
     </div>
