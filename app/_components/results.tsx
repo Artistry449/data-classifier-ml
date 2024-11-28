9a5d661221dce8cb34c2ef84375aeb0ceed7b003
enhpurevjawhlan <enhpurevjawhlan@gmail.com>
2024-11-28 13:20:38 +0800

    undes ugiin jagsaalt nemsen

--- app/_components/results.tsx
+++ app/_components/results.tsx
@@ -11,12 +11,14 @@ const results = ({
 }) => {
   return (
     <div className="h-full grid col-span-1 ml-3 md:w-1/4 gap-3">
-      <div className="border-gray-200 p-3 bg-white rounded-lg shadow row-span-3">
+      <div className="border-gray-200 p-3 bg-white rounded-lg shadow row-span-3 ">
         <h2 className="text-sm font-semibold pb-1">Алдаатай үгсийн жагсаалт</h2>
         <div className="w-full border bg-gray-700" />
-        {wrongWords.map((word, index) => {
-          return <div key={index}>{word}</div>;
-        })}
+        <div className="max-h-[200px] overflow-auto">
+          {wrongWords.map((word, index) => {
+            return <div key={index}>{word}</div>;
+          })}
+        </div>
       </div>
       <div className="border-gray-200 p-3 bg-white rounded-lg shadow row-span-2">
         <h2 className="text-sm font-semibold pb-1">Текстийн төрөл</h2>
--- app/_components/rootwords.tsx
+++ app/_components/rootwords.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const rootwords = ({ rootWords }: { rootWords: string }) => {
   return (
-    <div className="mt-4 border border-gray-300 bg-white rounded-lg shadow p-4">
+    <div className="mt-4 w-full max-w-6xl max-h-[100px] overflow-auto border border-gray-300 bg-white rounded-lg shadow p-4">
+      <p className="mb-4">Үндэс үгсийн жагсаалт</p>
       {rootWords}
     </div>
   );
